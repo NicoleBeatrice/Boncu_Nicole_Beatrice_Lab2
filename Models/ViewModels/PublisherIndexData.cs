9c9e6ac6fb3aa80f3e8e4d0fada43cd363323b97
NicoleBeatrice <nicole.boncu@stud.ubbcluj.ro>
2022-11-25 15:15:12 +0200

    Update

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿namespace Boncu_Nicole_Beatrice_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher>? Publishers { get; set; }
+        public IEnumerable<Book>? Books { get; set; }
+
+    }
+}
--- Pages/Publishers/Index.cshtml.cs
+++ Pages/Publishers/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Boncu_Nicole_Beatrice_Lab2.Data;
 using Boncu_Nicole_Beatrice_Lab2.Models;
+using Boncu_Nicole_Beatrice_Lab2.Models.ViewModels;
 
 namespace Boncu_Nicole_Beatrice_Lab2.Pages.Publishers
 {
@@ -18,14 +19,25 @@ namespace Boncu_Nicole_Beatrice_Lab2.Pages.Publishers
         {
             _context = context;
         }
+        public IList<Publisher> Publisher { get; set; } = default!;
+        public PublisherIndexData PublisherData { get; set; }
+        public int PublisherID { get; set; }
+        public int BookID { get; set; }
 
-        public IList<Publisher> Publisher { get;set; } = default!;
-
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            if (_context.Publisher != null)
+            PublisherData = new PublisherIndexData();
+            PublisherData.Publishers = await _context.Publisher
+                .Include(i => i.Books)
+                .ThenInclude(c => c.Author)
+                .OrderBy(i => i.PublisherName)
+                .ToListAsync();
+            if (id != null)
             {
-                Publisher = await _context.Publisher.ToListAsync();
+                PublisherID = id.Value;
+                Publisher publisher = PublisherData.Publishers
+                    .Where(i => i.ID == id.Value).Single();
+                PublisherData.Books = publisher.Books;
             }
         }
     }
