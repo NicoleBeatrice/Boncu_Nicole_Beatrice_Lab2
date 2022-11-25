ae076481d7cf6ee79e16f6372daa2a84640ffac4
NicoleBeatrice <nicole.boncu@stud.ubbcluj.ro>
2022-11-25 23:25:23 +0200

    Update

--- Migrations/20221125210050_Borrowing.Designer.cs
+++ Migrations/20221125210050_Borrowing.Designer.cs
@@ -4,6 +4,7 @@ using Boncu_Nicole_Beatrice_Lab2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Boncu_Nicole_Beatrice_Lab2.Migrations
 {
     [DbContext(typeof(Boncu_Nicole_Beatrice_Lab2Context))]
-    partial class Boncu_Nicole_Beatrice_Lab2ContextModelSnapshot : ModelSnapshot
+    [Migration("20221125210050_Borrowing")]
+    partial class Borrowing
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Pages/Borrowings/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Boncu_Nicole_Beatrice_Lab2.Data;
+using Boncu_Nicole_Beatrice_Lab2.Models;
+
+namespace Boncu_Nicole_Beatrice_Lab2.Pages.Borrowings
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Boncu_Nicole_Beatrice_Lab2.Data.Boncu_Nicole_Beatrice_Lab2Context _context;
+
+        public IndexModel(Boncu_Nicole_Beatrice_Lab2.Data.Boncu_Nicole_Beatrice_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Borrowing> Borrowing { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Borrowing != null)
+            {
+                Borrowing = await _context.Borrowing
+                .Include(b => b.Book)
+                  .ThenInclude(b => b.Author)
+                .Include(b => b.Member).ToListAsync();
+            }
+        }
+    }
+}
